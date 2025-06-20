6988584bcb5936ee5ee13002051fdffa1b7e2776
sevetom <sevetom2611@libero.it>
2025-06-20 17:36:51 +0200

    test: fix syntax errors in CollectableTest.scala

--- src/test/scala/it/unibo/party/model/items/CollectableTest.scala
+++ src/test/scala/it/unibo/party/model/items/CollectableTest.scala
@@ -6,15 +6,13 @@ class CollectableTest extends AnyFlatSpec:
   import Collectable.{Monad, Rung}
   import CollectableOperations.*
 
-  "A Monad" should "be collectible by any owned items" in {
+  "A Monad" should "be collectible by any owned items" in:
     val monad: Monad = Collectable.Monad()
     val owned: Seq[Collectable] = Seq(Collectable.Monad(), Collectable.Rung(1))
     assert(monad.collectibleBy(owned).isDefined)
-  }
 
-  "A Rung" should "be collectible if enough Monads are owned" in {
+  "A Rung" should "be collectible if enough Monads are owned" in:
     val rungPrice: Int = 2
     val rung: Rung = Rung(rungPrice)
     val owned: Seq[Collectable] = for i <- 1 to rungPrice yield Monad()
     assert(rung.collectibleBy(owned).isDefined)
-  }
