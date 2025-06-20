0e57870c6baec84cd8313e42b618c29c3a2c511f
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-06-20 15:39:52 +0200

    feat: add test boardBox should return an empty optional if an item is requested but not present

--- src/main/scala/it/unibo/party/model/GameBoard.scala
+++ src/main/scala/it/unibo/party/model/GameBoard.scala
@@ -3,14 +3,22 @@ package it.unibo.party.model
 import it.unibo.party.model.GameBoard.BoardBox.EmptyBox
 
 object GameBoard {
+  type Collectable
   
   enum BoardBox:
     case EmptyBox
+    case FullBox(item: Collectable)
     
   extension(b: BoardBox)
     def isEmpty: Boolean = b match 
       case EmptyBox => true
       case _ => false
+      
+    def tryAcquireItem: Option[Collectable] = b match 
+      case EmptyBox => Option.empty
+      case _ => ???
+    
+      
     
 
 
--- src/test/scala/it/unibo/party/model/GameBoardTest.scala
+++ src/test/scala/it/unibo/party/model/GameBoardTest.scala
@@ -4,14 +4,16 @@ import org.scalatest.*
 import flatspec.*
 import it.unibo.party.model.GameBoard.BoardBox
 import it.unibo.party.model.GameBoard.BoardBox.*
-
 import matchers.*
 
 class GameBoardTest extends AnyFlatSpec with should.Matchers:
+  val emptyBox: BoardBox = EmptyBox
 
   "A BoardBox" should "not have items if empty" in:
-    val box: BoardBox = EmptyBox
-    box.isEmpty should be (true)
+    emptyBox.isEmpty should be (true)
+
+  it should "return an empty optional if an item is requested but not present" in:
+    emptyBox.tryAcquireItem should be (Option.empty)
 
 
 
