9248ea957b08ac66a1a62c9a0d38ea13f7d83978
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-06-21 11:49:46 +0200

    feat: add test GameBoard should Throw IllegalArgumentException when trying to move a non-existing pown

--- src/test/scala/it/unibo/party/model/board/GameBoardTest.scala
+++ src/test/scala/it/unibo/party/model/board/GameBoardTest.scala
@@ -13,20 +13,26 @@ import org.scalatest.matchers.*
 class GameBoardTest extends AnyFlatSpec with should.Matchers:
   val pawnId = 1
   val pawnStratPosition: BoardPosition = BoardPosition(0, 0)
+  val existentPosition: BoardPosition = BoardPosition(1, 0)
   val simpleBoard: GameBoard = GameBoard(
     Map(BoardPosition(0, 0) -> EmptyBox, BoardPosition(1, 0) -> EmptyBox),
     Map(pawnId -> Pawn[BoardPosition](pawnStratPosition, Pocket.empty))
   )
 
   "A GameBoard" should "allow to move a pawn in an existent position" in:
-    val existentPosition: BoardPosition = BoardPosition(1, 0)
     val newBoard = simpleBoard.movePawn(pawnId, existentPosition)
     newBoard.pawns(pawnId).position should be (existentPosition)
     
-  it should "Throw IllegalArgumentException when trying to move a pown to a non-existent position" in:
+  it should "Throw IllegalArgumentException when trying to move a pawn to a non-existent position" in:
     val nonExistentPosition: BoardPosition = BoardPosition(2, 2)
     assertThrows[IllegalArgumentException]:
       simpleBoard.movePawn(pawnId, nonExistentPosition)
+      
+  it should "Throw IllegalArgumentException when trying to move a non-existing pown" in:
+    val nonExistentPawnId = 5
+    assertThrows[IllegalArgumentException]:
+      simpleBoard.movePawn(nonExistentPawnId, existentPosition)
+    
     
     
 
