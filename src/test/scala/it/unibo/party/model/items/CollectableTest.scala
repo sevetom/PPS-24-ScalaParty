5cbc297a63892ba077a28179a52dbb6497413287
sevetom <sevetom2611@libero.it>
2025-06-21 12:49:53 +0200

    feat: improve assertions in CollectableTest using matchers

--- src/test/scala/it/unibo/party/model/items/CollectableTest.scala
+++ src/test/scala/it/unibo/party/model/items/CollectableTest.scala
@@ -1,6 +1,7 @@
 package it.unibo.party.model.items
 
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
 
 class CollectableTest extends AnyFlatSpec:
   import Collectable.{Monad, Rung}
@@ -9,10 +10,10 @@ class CollectableTest extends AnyFlatSpec:
   "A Monad" should "be collectible by any owned items" in:
     val monad: Monad = Collectable.Monad()
     val owned: Seq[Collectable] = Seq(Collectable.Monad(), Collectable.Rung(1))
-    assert(monad.collectibleBy(owned).isDefined)
+    monad.collectibleBy(owned).isDefined shouldBe true
 
   "A Rung" should "be collectible if enough Monads are owned" in:
     val rungPrice: Int = 2
     val rung: Rung = Rung(rungPrice)
     val owned: Seq[Collectable] = for i <- 1 to rungPrice yield Monad()
-    assert(rung.collectibleBy(owned).isDefined)
+    rung.collectibleBy(owned).isDefined shouldBe true
