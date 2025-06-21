91563623a1c013b558bf80bfffbe2732af23f58f
sevetom <sevetom2611@libero.it>
2025-06-21 14:07:19 +0200

    feat: implement type related operations in Pocket to work with multiple Collectable

--- src/main/scala/it/unibo/party/model/player/Pocket.scala
+++ src/main/scala/it/unibo/party/model/player/Pocket.scala
@@ -1,19 +1,24 @@
 package it.unibo.party.model.player
 
-import it.unibo.party.model.items.Collectable
+import it.unibo.party.model.items.{Collectable, CollectableType}
+import it.unibo.party.model.items.CollectableOperations.getType
 
 trait Pocket:
   def getAll: Seq[Collectable]
   
-  def getByType(item: Collectable): Seq[Collectable]
+  def getByType(itemType: CollectableType): Seq[Collectable]
 
   def add(item: Collectable): Pocket
 
   def remove(item: Collectable): Pocket
 
+  def addMultiple(item: Collectable, count: Int): Pocket
+
+  def removeMultipleByType(itemType: CollectableType, count: Int): Pocket
+
   def contains(item: Collectable): Boolean
 
-  def countByType(item: Collectable): Int
+  def countByType(itemType: CollectableType): Int
 
   def isEmpty: Boolean
 
@@ -25,15 +30,24 @@ object Pocket:
   private case class PocketImpl(items: Seq[Collectable]) extends Pocket:
     override def getAll: Seq[Collectable] = items
     
-    override def getByType(item: Collectable): Seq[Collectable] = items.filter(_.getClass == item.getClass)
+    override def getByType(itemType: CollectableType): Seq[Collectable] = items.filter(_.getType == itemType)
 
     override def add(item: Collectable): Pocket = Pocket(items :+ item)
 
     override def remove(item: Collectable): Pocket = Pocket(items.filter(_ != item))
 
+    override def addMultiple(item: Collectable, count: Int): Pocket = Pocket(items ++ Seq.fill(count)(item))
+
+    override def removeMultipleByType(itemType: CollectableType, count: Int): Pocket =
+      val typedItems = items.filter(_.getType == itemType)
+      if typedItems.size <= count then
+        Pocket(items.filterNot(_.getType == itemType))
+      else
+        Pocket(items.diff(typedItems.take(count)))
+
     override def contains(item: Collectable): Boolean = items.contains(item)
 
-    override def countByType(item: Collectable): Int = items.count(_.getClass == item.getClass)
+    override def countByType(itemType: CollectableType): Int = items.count(_.getType == itemType)
 
     override def isEmpty: Boolean = items.isEmpty
 
--- src/test/scala/it/unibo/party/model/player/PocketTest.scala
+++ src/test/scala/it/unibo/party/model/player/PocketTest.scala
@@ -1,41 +1,56 @@
 package it.unibo.party.model.player
 
-import it.unibo.party.model.items.Collectable
+import it.unibo.party.model.items.{Collectable, CollectableType}
+import it.unibo.party.model.items.CollectableOperations.*
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 
 class PocketTest extends AnyFlatSpec:
-  val pocket: Pocket = Pocket.empty
+  val emptyPocket: Pocket = Pocket.empty
   val genericItem: Collectable = Collectable.Rung(1)
   val differentItemWithSameType: Collectable = Collectable.Rung(2)
 
-  "A Pocket" should "be empty when it has no items" in:
-    pocket.isEmpty shouldBe true
+  "A Pocket" should "be empty when it has no items" in :
+    emptyPocket.isEmpty shouldBe true
 
-  it should "have the correct size" in:
-    pocket.size shouldEqual 0
+  it should "have the correct size" in :
+    emptyPocket.size shouldEqual 0
 
   it should "increase size when adding items" in :
-    val updatedPocket = pocket.add(genericItem)
+    val updatedPocket = emptyPocket.add(genericItem)
     updatedPocket.size shouldEqual 1
 
-  it should "decrease size when removing items" in:
-    val updatedPocket = pocket.add(genericItem).remove(genericItem)
+  it should "decrease size when removing items" in :
+    val updatedPocket = emptyPocket.add(genericItem).remove(genericItem)
     updatedPocket.size shouldEqual 0
 
-  it should "allow retrieving all items" in:
-    val updatedPocket = pocket.add(genericItem)
+  it should "allow retrieving all items" in :
+    val updatedPocket = emptyPocket.add(genericItem)
     updatedPocket.getAll shouldBe Seq(genericItem)
 
   it should "contain an item after it is added" in :
-    val updatedPocket = pocket.add(genericItem)
+    val updatedPocket = emptyPocket.add(genericItem)
     updatedPocket.contains(genericItem) shouldBe true
 
   it should "allow retrieving items by type" in :
-    val updatedPocket = pocket.add(genericItem).add(differentItemWithSameType)
-    val expectedItems = updatedPocket.getAll.filter(_.getClass == genericItem.getClass)
-    updatedPocket.getByType(genericItem) should contain theSameElementsAs expectedItems
+    val updatedPocket = emptyPocket.add(genericItem).add(differentItemWithSameType)
+    val expectedItems = updatedPocket.getAll.filter(_.getType == genericItem.getType)
+    updatedPocket.getByType(genericItem.getType) should contain theSameElementsAs expectedItems
 
-  it should "allow counting items by type" in:
-    val updatedPocket = pocket.add(genericItem).add(differentItemWithSameType)
-    updatedPocket.countByType(genericItem) shouldEqual updatedPocket.getByType(genericItem).size
+  it should "allow counting items by type" in :
+    val updatedPocket = emptyPocket.add(genericItem).add(differentItemWithSameType)
+    updatedPocket.countByType(genericItem.getType) shouldEqual updatedPocket.getByType(genericItem.getType).size
+
+  it should "allow adding multiple items" in :
+    val addCount = 3
+    val updatedPocket = emptyPocket.addMultiple(genericItem, addCount)
+    updatedPocket.size shouldEqual addCount
+
+  it should "allow removing multiple items by type" in :
+    val removeCount = 2
+    val totalCount = 5
+    val updatedPocket = emptyPocket.addMultiple(genericItem, totalCount)
+        .removeMultipleByType(genericItem.getType, removeCount)
+    updatedPocket.countByType(genericItem.getType) shouldEqual totalCount - removeCount
+    
+  
