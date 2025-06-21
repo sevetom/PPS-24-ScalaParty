0562b9eab00074f8b80385a2df2053765d29949c
sevetom <sevetom2611@libero.it>
2025-06-21 12:41:48 +0200

    test: improve assertions in PawnTest and PocketTest using matchers

--- src/test/scala/it/unibo/party/model/player/PawnTest.scala
+++ src/test/scala/it/unibo/party/model/player/PawnTest.scala
@@ -3,23 +3,18 @@ package it.unibo.party.model.player
 import it.unibo.party.geometry.Point2D
 import it.unibo.party.model.items.Collectable
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
 
 class PawnTest extends AnyFlatSpec:
   val initialPosition: Point2D[Int] = Point2D(0, 0)
   val pawn: Pawn[Point2D[Int]] = Pawn.emptyPockets(initialPosition)
   
-  "A starting Pawn" should "have an initial position and an empty pocket" in:
-    assert(pawn.position == initialPosition)
-    assert(pawn.pocket.isEmpty)
-  
   "A Pawn" should "allow moving to a new position" in:
     val newPosition: Point2D[Int] = Point2D(1, 1)
     val movedPawn: Pawn[Point2D[Int]] = pawn moveTo newPosition
-    assert(movedPawn.position == newPosition)
-    assert(movedPawn.pocket == pawn.pocket)
+    movedPawn.position shouldEqual newPosition
 
   it should "allow updating its pocket" in:
     val newPocket: Pocket = Pocket.empty.add(Collectable.Monad())
     val updatedPawn: Pawn[Point2D[Int]] = pawn withPocket newPocket
-    assert(updatedPawn.pocket == newPocket)
-    assert(updatedPawn.position == pawn.position)
+    updatedPawn.pocket shouldEqual newPocket
--- src/test/scala/it/unibo/party/model/player/PocketTest.scala
+++ src/test/scala/it/unibo/party/model/player/PocketTest.scala
@@ -1,8 +1,8 @@
 package it.unibo.party.model.player
 
 import it.unibo.party.model.items.Collectable
-import it.unibo.party.model.player.Pocket
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
 
 class PocketTest extends AnyFlatSpec:
   val pocket: Pocket = Pocket.empty
@@ -10,36 +10,32 @@ class PocketTest extends AnyFlatSpec:
   val differentItemWithSameType: Collectable = Collectable.Rung(2)
 
   "A Pocket" should "be empty when it has no items" in:
-    assert(pocket.isEmpty)
-    assert(pocket.size == 0)
+    pocket.isEmpty shouldBe true
 
-  it should "allow adding items" in:
+  it should "have the correct size" in:
+    pocket.size shouldEqual 0
+
+  it should "increase size when adding items" in :
     val updatedPocket = pocket.add(genericItem)
-    assert(!updatedPocket.isEmpty)
-    assert(updatedPocket.size == 1)
-    assert(updatedPocket.contains(genericItem))
+    updatedPocket.size shouldEqual 1
 
-  it should "allow removing items" in:
+  it should "decrease size when removing items" in:
     val updatedPocket = pocket.add(genericItem).remove(genericItem)
-    assert(updatedPocket.isEmpty)
-    assert(updatedPocket.size == 0)
-
-  it should "allow checking for item existence" in:
-    val updatedPocket = pocket.add(genericItem)
-    assert(updatedPocket.contains(genericItem))
-    assert(!updatedPocket.contains(differentItemWithSameType))
-
-  it should "allow counting items by type" in:
-    val updatedPocket = pocket.add(genericItem).add(differentItemWithSameType)
-    assert(updatedPocket.countByType(genericItem) == 2)
-    assert(updatedPocket.countByType(differentItemWithSameType) == 2)
+    updatedPocket.size shouldEqual 0
 
   it should "allow retrieving all items" in:
     val updatedPocket = pocket.add(genericItem)
-    assert(updatedPocket.getAll.contains(genericItem))
-    assert(updatedPocket.getAll.size == 1)
+    updatedPocket.getAll shouldBe Seq(genericItem)
 
-  it should "allow retrieving items by type" in:
+  it should "contain an item after it is added" in :
+    val updatedPocket = pocket.add(genericItem)
+    updatedPocket.contains(genericItem) shouldBe true
+
+  it should "allow retrieving items by type" in :
     val updatedPocket = pocket.add(genericItem).add(differentItemWithSameType)
-    assert(updatedPocket.getByType(genericItem).size == 2)
-    assert(updatedPocket.getByType(differentItemWithSameType).size == 2)
+    val expectedItems = updatedPocket.getAll.filter(_.getClass == genericItem.getClass)
+    updatedPocket.getByType(genericItem) should contain theSameElementsAs expectedItems
+
+  it should "allow counting items by type" in:
+    val updatedPocket = pocket.add(genericItem).add(differentItemWithSameType)
+    updatedPocket.countByType(genericItem) shouldEqual updatedPocket.getByType(genericItem).size
