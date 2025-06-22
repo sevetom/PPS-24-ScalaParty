385402f8a1b7681a3f95aed47a7e3c7c2aba74cb
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-06-22 11:04:46 +0200

    refactor: add Dice scalafile with Dice and CanRoll traits and implementations

---
+++ src/main/scala/it/unibo/party/model/partyGame/Dice.scala
@@ -0,0 +1,22 @@
+package it.unibo.party.model.partyGame
+
+trait CanRoll[T]:
+  extension (t: T) def roll(n: Int): (T, List[Int])
+
+trait Dice:
+  def lastRolled: List[Int]
+
+  def roll(n: Int): (Dice, List[Int])
+
+object Dice:
+  def apply(): Dice = DiceImpl(List.empty)
+
+  private val DICE_DIM = 6
+
+  private case class DiceImpl(_lastRolled: List[Int]) extends Dice:
+    override def lastRolled: List[Int] = _lastRolled
+
+    override def roll(n: Int): (Dice, List[Int]) =
+      require(n > 0, "Number of rolls must be positive")
+      val newRoll = List.fill(n)(scala.util.Random.nextInt(DICE_DIM) + 1)
+      (DiceImpl(newRoll), newRoll)
--- src/main/scala/it/unibo/party/model/partyGame/PartyGame.scala
+++ src/main/scala/it/unibo/party/model/partyGame/PartyGame.scala
@@ -21,25 +21,3 @@ object PartyGame:
       def roll(n: Int): (PartyGame, List[Int]) =
         val (newDice, result) = pg.dice.roll(n)
         (PartyGame(pg.board)(using newDice), result)
-
-trait CanRoll[T]:
-  extension (t: T) def roll(n: Int): (T, List[Int])
-
-trait Dice:
-  def lastRolled: List[Int]
-  def roll(n: Int): (Dice, List[Int])
-
-object Dice:
-  def apply(): Dice = DiceImpl(List.empty)
-
-  private val DICE_DIM = 6
-
-  private case class DiceImpl(_lastRolled: List[Int]) extends Dice:
-    override def lastRolled: List[Int] = _lastRolled
-
-    override def roll(n: Int): (Dice, List[Int]) =
-      require(n > 0, "Number of rolls must be positive")
-      val newRoll = List.fill(n)(scala.util.Random.nextInt(DICE_DIM) + 1)
-      (DiceImpl(newRoll), newRoll)
-
-
