d0735cd627ca51fe04b8ce73d6ef38faf3cefba0
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-06-22 17:46:49 +0200

    feat: new home screen with Scala fx

--- src/main/scala/it/unibo/party/view/GameView.scala
+++ src/main/scala/it/unibo/party/view/GameView.scala
@@ -1,16 +1,18 @@
 package it.unibo.party.view
 
-import it.unibo.party.view.screens.StartingScreen
+import it.unibo.party.view.screens.StartingScene
+import scalafx.application.JFXApp3
 
 import scala.swing.*
 import java.awt.Dimension
 
-object GameView extends SimpleSwingApplication {
-  def top: Frame = new MainFrame {
-    title = "Scala Party"
-    preferredSize = new Dimension(1440, 1024)
-    resizable = false
+object GameView extends JFXApp3:
+  override def start(): Unit =
+    stage = new JFXApp3.PrimaryStage:
+      title = "Scala Party"
+      width = 1440
+      height = 1024
+      resizable = false
+      scene = new StartingScene
+  
 
-    contents = StartingScreen()
-  }
-}
