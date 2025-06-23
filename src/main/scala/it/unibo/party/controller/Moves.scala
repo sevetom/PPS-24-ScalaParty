89f1c4150d4760a30157805a058d0636b61e265b
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-06-23 16:38:24 +0200

    feat: enhance OpponentLogic to handle game phases

--- src/main/scala/it/unibo/party/controller/Moves.scala
+++ src/main/scala/it/unibo/party/controller/Moves.scala
@@ -1,5 +1,7 @@
 package it.unibo.party.controller
 
+import it.unibo.party.geometry.Point2D
+
 object Moves:
   
   enum PartyMoveType:
@@ -8,5 +10,7 @@ object Moves:
   case class PartyMove(
     playerId: Int,
     moveType: PartyMoveType,
+    diceResult: Option[Int] = None,
+    position: Option[Point2D[Int]] = None,
     // direction: Option[Direction]
   )
--- src/main/scala/it/unibo/party/controller/OpponentLogic.scala
+++ src/main/scala/it/unibo/party/controller/OpponentLogic.scala
@@ -1,17 +1,40 @@
 package it.unibo.party.controller
 
+import it.unibo.party.common.GameState.GamePhase
+import it.unibo.party.controller.Moves.PartyMove
+import it.unibo.party.geometry.Point2D
+import it.unibo.party.model.partyGame.Dice
+
+sealed trait OpponentActionResult
+
+object OpponentActionResult:
+  case class Rolled(values: List[Int]) extends OpponentActionResult
+
+  case class Turn() extends OpponentActionResult
+
+  case class Moved(position: Point2D[Int]) extends OpponentActionResult
+
+  case class Default() extends OpponentActionResult
+
 trait OpponentLogic:
-  def update(gamePhase: GamePhase): Boolean
+  def update(gamePhase: GamePhase, move: PartyMove): OpponentActionResult
 
 object OpponentLogic:
   def apply(): OpponentLogic = OpponentLogicImpl()
 
   private case class OpponentLogicImpl() extends OpponentLogic:
-    override def update(gamePhase: GamePhase): Boolean =
+    override def update(gamePhase: GamePhase, move: PartyMove): OpponentActionResult =
       gamePhase match
-        case GamePhase.INITIAL_ROLL => true
-        case GamePhase.PLAYERS_TURN => true
-        case GamePhase.PLAYER_MOVING => true
-        case GamePhase.PLAYING_MINIGAME => true
-        case GamePhase.GAME_OVER => true
-        case _ => false // Handle other phases as needed
+        case GamePhase.GAME_START =>
+          val dice = Dice()
+          val (_, result) = dice.roll()
+          OpponentActionResult.Rolled(result)
+        case GamePhase.DICE_ROLL =>
+          val dice = Dice()
+          val (_, result) = dice.roll()
+          OpponentActionResult.Rolled(result)
+        case GamePhase.PLAYER_MOVING =>
+          OpponentActionResult.Moved(move.position.getOrElse(Point2D(0, 0)) +
+            Point2D(move.diceResult.getOrElse(0), 0)) // Assuming a simple rightward movement for demonstration
+        case _ =>
+          OpponentActionResult.Default()
