a91b760c3a18a59644da5b682621f6339808a920
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-06-23 17:39:55 +0200

    feat: temporary implement PlayingAgent

---
+++ src/main/scala/it/unibo/party/controller/PlayingAgent.scala
@@ -0,0 +1,17 @@
+package it.unibo.party.controller
+
+import it.unibo.party.controller.Moves.PartyMove
+
+trait PlayingAgent:
+  def id: Int
+// def controller: PartyController
+  
+  def makeMove(move: PartyMove): Unit =
+    ???
+    // controller.handleMove(move)
+
+object PlayingAgent:
+  
+  def apply(id: Int /*, controller: PartyController*/): PlayingAgent = PlayingAgentImpl(id/*, controller */)
+
+  private case class PlayingAgentImpl(id: Int /*, controller: PartyController*/) extends PlayingAgent
