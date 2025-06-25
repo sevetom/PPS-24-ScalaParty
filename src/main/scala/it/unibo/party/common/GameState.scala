049c03fe9c4cb0f89e7c7523d6ff6a0fc3fcfc23
sevetom <sevetom2611@libero.it>
2025-06-25 21:27:22 +0200

    feat: useful constructor for gamestate starting from game reference

--- src/main/scala/it/unibo/party/common/GameState.scala
+++ src/main/scala/it/unibo/party/common/GameState.scala
@@ -1,18 +1,39 @@
 package it.unibo.party.common
 
-import it.unibo.party.geometry.Point2D
+import it.unibo.party.geometry.{Direction, Point2D}
 import it.unibo.party.model.items.Collectable
+import it.unibo.party.model.partyGame.PartyGame
 
+enum GamePhase:
+  case GameStart, DiceRoll, PlayerMoving, PlayingMinigame
+
+case class GameState(
+                      phase: GamePhase,
+                      currentPlayer: Int,
+                      diceResult: Option[Int] = Option.empty,
+                      possibleDirections: Option[Set[Direction]] = Option.empty,
+                      itemsCollected: Map[Int, Set[Collectable]],
+                      board: Set[Point2D[Int]],
+                      playersPositions: Map[Point2D[Int], Int],
+                      itemsPositions: Map[Point2D[Int], Collectable],
+                    )
 
 object GameState:
-  
-  enum GamePhase:
-    case GameStart, DiceRoll, PlayerMoving, PlayingMinigame
-    
-  case class GameState(  
-    phase: GamePhase,
-    currentPlayer: Int,
-    board: Seq[Point2D[Int]],
-    playersPositions: Seq[(Int, Point2D[Int])],
-    itemsPositions: Seq[(Collectable, Point2D[Int])],
-  )
+  def fromGame(
+                game: PartyGame,
+                gamePhase: GamePhase,
+                playerTurn: Int,
+                diceResult: Option[Int] = Option.empty,
+                possibleDirections: Option[Set[Direction]] = Option.empty
+              ):
+  GameState =
+    GameState(
+      phase = gamePhase,
+      currentPlayer = playerTurn,
+      diceResult = diceResult,
+      possibleDirections = possibleDirections,
+      itemsCollected = game.getPocketsContents,
+      board = game.getBoardBoxes.map(_.toPoint2D),
+      playersPositions = game.getPlayersPosition.map((pos, playerId) => pos.toPoint2D -> playerId),
+      itemsPositions = game.getItems.map((pos, item) => pos.toPoint2D -> item)
+    )
