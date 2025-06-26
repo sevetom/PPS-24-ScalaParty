0cfd6269635f330d8d86441a281c9f43516ee28b
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-06-26 14:22:10 +0200

    feat: implement OpponentLogic with updated game state handling

--- src/main/scala/it/unibo/party/controller/OpponentLogic.scala
+++ src/main/scala/it/unibo/party/controller/OpponentLogic.scala
@@ -1,45 +1,20 @@
-//package it.unibo.party.controller
-//
-//
-//
-//import it.unibo.party.common.GameState
-//import it.unibo.party.controller.Moves.PartyMove
-//import it.unibo.party.geometry.Point2D
-//import it.unibo.party.model.items.Collectable
-//import it.unibo.party.model.partyGame.Dice
-//
-//sealed trait OpponentActionResult
-//
-//object OpponentActionResult:
-//  case class Rolled(values: List[Int]) extends OpponentActionResult
-//
-//  case class Turn() extends OpponentActionResult
-//
-//  case class Moved(position: Point2D[Int], collected: Option[List[Collectable]]) extends OpponentActionResult
-//
-//  case class Default() extends OpponentActionResult
-//
-//trait OpponentLogic:
-//  def update(gameState: GameState): OpponentActionResult
-//
-//object OpponentLogic:
-//  def apply(): OpponentLogic = OpponentLogicImpl()
-//
-//  private case class OpponentLogicImpl() extends OpponentLogic:
-//    override def update(gamePhase: GamePhase, move: PartyMove): OpponentActionResult =
-//      gamePhase match
-//        case GamePhase.GAME_START =>
-//          val dice = Dice()
-//          val (_, result) = dice.roll()
-//          OpponentActionResult.Rolled(result)
-//        case GamePhase.DICE_ROLL =>
-//          val dice = Dice()
-//          val (_, result) = dice.roll()
-//          OpponentActionResult.Rolled(result)
-//        case GamePhase.PLAYER_MOVING =>
-//          val startPosition = move.position.getOrElse(Point2D(0, 0))
-//          val newPosition = Point2D(startPosition.x + move.diceResult.getOrElse(0), startPosition.y)
-//          val collectedItems = List.empty[Collectable] // Simulate no items collected for simplicity
-//          OpponentActionResult.Moved(newPosition, Some(collectedItems))
-//        case _ =>
-//          OpponentActionResult.Default()
+package it.unibo.party.controller
+
+import it.unibo.party.common.{GamePhase, GameState}
+import it.unibo.party.controller.Moves.{PartyMove, PartyMoveType}
+import it.unibo.party.geometry.Direction
+
+trait OpponentLogic:
+  def update(state: GameState): Unit
+
+object OpponentLogic:
+
+  def apply(id: Int, playingAgent: PlayingAgent): OpponentLogic = OpponentLogicImpl(id, playingAgent)
+
+  private case class OpponentLogicImpl(id: Int, playingAgent: PlayingAgent) extends OpponentLogic:
+    override def update(state: GameState): Unit =
+      (state.phase, state.currentPlayer) match
+        case (GamePhase.PlayerMoving, 1) =>
+          playingAgent.makeMove(PartyMove(id, PartyMoveType.Movement, Some(Direction.Right)))
+        case _ =>
+          () //placeholder
