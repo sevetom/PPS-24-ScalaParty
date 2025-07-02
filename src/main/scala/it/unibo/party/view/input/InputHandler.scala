0e905927475bb1fa15fce7e7f033afa53aae261f
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-07-02 17:43:16 +0200

    feat: add handling for Space key to trigger DiceRoll action (only for testing purpose)

--- src/main/scala/it/unibo/party/view/input/InputHandler.scala
+++ src/main/scala/it/unibo/party/view/input/InputHandler.scala
@@ -2,7 +2,7 @@ package it.unibo.party.view.input
 
 import it.unibo.party.common.{GamePhase, GameState}
 import it.unibo.party.controller.Moves.PartyMove
-import it.unibo.party.controller.Moves.PartyMoveType.Movement
+import it.unibo.party.controller.Moves.PartyMoveType.*
 import it.unibo.party.controller.PlayingAgent
 import it.unibo.party.geometry.Direction
 import scalafx.scene.input.{KeyCode, KeyEvent}
@@ -17,6 +17,8 @@ object InputHandler:
         case KeyCode.Down.delegate if directions.contains(Direction.Down) => agent.makeMove(PartyMove(state.currentPlayer, Movement, Some(Direction.Down)))
         case KeyCode.Left.delegate if directions.contains(Direction.Left) => agent.makeMove(PartyMove(state.currentPlayer, Movement, Some(Direction.Left)))
         case KeyCode.Right.delegate if directions.contains(Direction.Right) => agent.makeMove(PartyMove(state.currentPlayer, Movement, Some(Direction.Right)))
+        case KeyCode.Space.delegate =>
+          agent.makeMove(PartyMove(state.currentPlayer, DiceRoll, Option.empty[Direction]))
         case _ => // Ignore other keys
   }
       
