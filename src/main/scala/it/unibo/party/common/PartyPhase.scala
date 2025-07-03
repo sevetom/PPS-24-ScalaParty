ce3a922fe030b36d42b72478a29a69bcf845a15f
sevetom <sevetom2611@libero.it>
2025-07-03 16:57:12 +0200

    fest: create PartyTurnManager for better separation of responsability

---
+++ src/main/scala/it/unibo/party/common/PartyPhase.scala
@@ -0,0 +1,5 @@
+package it.unibo.party.common
+
+enum PartyPhase:
+  case StartingRoll, DiceRoll, PlayerMoving, PlayingMinigame, GameOver
+  
--- src/main/scala/it/unibo/party/controller/PartyController.scala
+++ src/main/scala/it/unibo/party/controller/PartyController.scala
@@ -1,8 +1,9 @@
 package it.unibo.party.controller
 
-import it.unibo.party.common.GamePhase.StartingRoll
-import it.unibo.party.common.{GamePhase, GameState}
+import it.unibo.party.common.PartyPhase.StartingRoll
+import it.unibo.party.common.{PartyPhase, PartyState, Player}
 import it.unibo.party.controller.Moves.{PartyMove, PartyMoveType}
+import it.unibo.party.controller.managers.PartyTurnManager
 import it.unibo.party.controller.pubsub.{Publisher, Subscriber}
 import it.unibo.party.geometry.Direction
 import it.unibo.party.model.partyGame.{Dice, MovementResult, PartyGame}
@@ -12,43 +13,41 @@ private val stepsPerPlayer: Int = 1
 private val winRungs: Int = 1
 
 trait PartyController:
-  def addViewListener(listener: Subscriber[GameState]): Unit
+  def addViewListener(listener: Subscriber[PartyState]): Unit
 
-  def addMoveListener(listener: Subscriber[GameState]): Unit
+  def addMoveListener(listener: Subscriber[PartyState]): Unit
 
   def start(): Unit
 
   def handleMove(move: PartyMove): Unit
 
 object PartyController:
-  def apply(game: PartyGame, players: List[Int]) = new PartyControllerImpl(game, players)
+  def apply(game: PartyGame, players: List[Player]) = PartyControllerImpl(game, players)
 
-  class PartyControllerImpl(private var game: PartyGame, private var players: List[Int]) extends PartyController:
-    private var statePublisher: Publisher[GameState] = Publisher(List.empty)
-    private var gamePhase: GamePhase = GamePhase.StartingRoll
-    private var currentPlayerIndex: Int = 0
-    private var turnPlayer = players(currentPlayerIndex)
-    private var diceResults: Map[Int, Int] = Map.empty
+  class PartyControllerImpl(private var game: PartyGame, private var players: List[Player]) extends PartyController:
+    private var statePublisher: Publisher[PartyState] = Publisher(List.empty)
+    private var turnManager: PartyTurnManager = PartyTurnManager.fromTheStart(players)
+    private var diceResults: Map[Player, Int] = Map.empty
 
-    override def addViewListener(listener: Subscriber[GameState]): Unit =
+    override def addViewListener(listener: Subscriber[PartyState]): Unit =
       statePublisher = statePublisher.subscribeFirst(listener)
 
-    override def addMoveListener(listener: Subscriber[GameState]): Unit =
+    override def addMoveListener(listener: Subscriber[PartyState]): Unit =
       statePublisher = statePublisher.subscribeLast(listener)
 
     override def start(): Unit =
       statePublisher.publish(
-        GameState.fromGame(
+        PartyState.fromGame(
           game,
-          gamePhase,
-          turnPlayer
+          turnManager.currentPhase,
+          turnManager.currentPlayer
         )
       )
 
     override def handleMove(move: PartyMove): Unit =
       var directions: Option[Set[Direction]] = Option.empty
-      var diceResult: Option[(Int, Int)] = Option.empty
-      if move.playerId == turnPlayer then
+      var diceResult: Option[(Player, Int)] = Option.empty
+      if move.playerId == turnManager.currentPlayer.id then
         move.moveType match
           case PartyMoveType.Movement =>
             directions = Some(handleMovement(move.direction.getOrElse(Direction.Up)))
@@ -56,39 +55,33 @@ object PartyController:
             diceResult = Some(handleStartRoll())
         handleWinCondition()
         statePublisher.publish(
-          GameState.fromGame(
+          PartyState.fromGame(
             game,
-            gamePhase,
-            turnPlayer,
+            turnManager.currentPhase,
+            turnManager.currentPlayer,
             diceResult,
             directions,
           )
         )
 
     private def handleMovement(direction: Direction): Set[Direction] =
-      val result = game.movePlayer(currentPlayerIndex, direction, stepsPerPlayer)
+      val result = game.movePlayer(turnManager.currentPlayer.id, direction, stepsPerPlayer)
       result match
         case MovementResult.Moved(updatedGame) =>
           game = updatedGame
-          currentPlayerIndex += 1
-          currentPlayerIndex = if currentPlayerIndex < players.length then currentPlayerIndex else 0
-          turnPlayer = players(currentPlayerIndex)
-      game.getPossibleDirections(turnPlayer)
+          turnManager = turnManager.nextTurn()
+      game.getPossibleDirections(turnManager.currentPlayer.id)
 
-    private def handleStartRoll(): (Int, Int) =
-      val dicePlayer = turnPlayer
+    private def handleStartRoll(): (Player, Int) =
+      val dicePlayer = turnManager.currentPlayer
       val diceResult = Dice().roll()._2.head
-      diceResults = diceResults + (turnPlayer -> diceResult)
-      currentPlayerIndex += 1
-      if currentPlayerIndex >= players.size then
-        gamePhase = GamePhase.DiceRoll
-        currentPlayerIndex = 0
-        players = diceResults.toList.sortWith(_._2 > _._2).map(_._1)
-      turnPlayer = players(currentPlayerIndex)
-      (turnPlayer, diceResult)
+      diceResults = diceResults + (dicePlayer -> -diceResult)
+      turnManager = turnManager.nextTurn()
+      if turnManager.currentPhase != StartingRoll then
+        turnManager = turnManager.changePlayerOrder(diceResults)
+      (turnManager.currentPlayer, diceResult)
 
     private def handleWinCondition(): Unit =
       val winner = game.getPockets.find((k, v) => v.countByType(RungType) >= winRungs)
       if winner.isDefined then
-        gamePhase = GamePhase.GameOver
-        turnPlayer = winner.get._1
+        turnManager.end(Player(winner.get._1))
