dc12bb55b86e82fd2be143b7cfb0f7ed3e569aee
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-07-05 15:44:32 +0200

    feat: refine miniController

--- src/main/scala/it/unibo/party/controller/MiniController.scala
+++ src/main/scala/it/unibo/party/controller/MiniController.scala
@@ -4,12 +4,16 @@ import it.unibo.party.common.State
 import it.unibo.party.controller.Moves.Move
 
 trait MiniController:
+  def state: State
+  
   def start(): MiniController
-  def handleMove(move: Move): (MiniController, State)
+  def handleMove(move: Move): MiniController
 
 trait TimedMiniController extends MiniController:
   def startTime: Long
   def winTime: Long
+
+  def isTimeUp: Boolean = System.currentTimeMillis() - startTime >= winTime
 
 //object PuzzleController:
 //
