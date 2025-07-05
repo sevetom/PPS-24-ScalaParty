f53dcebab17edc0bbea2936697edd0d15ceb5906
sevetom <sevetom2611@libero.it>
2025-07-05 15:26:12 +0200

    feat: add space key for rolling dice

--- src/main/scala/it/unibo/party/view/input/InputHandler.scala
+++ src/main/scala/it/unibo/party/view/input/InputHandler.scala
@@ -9,18 +9,24 @@ import scalafx.scene.input.{KeyCode, KeyEvent}
 
 object InputHandler:
 
-  def apply(event: KeyEvent, state: PartyState, agent: PlayingAgent): Unit = 
-    val directions = state.possibleDirections.getOrElse(Set.empty[Direction])
-    if state.currentPlayer.id == agent.id && state.phase == PartyPhase.PlayerMoving then
-      event.getCode match
-        case KeyCode.Up.delegate if directions.contains(Direction.Up) => 
-          agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Up)))
-        case KeyCode.Down.delegate if directions.contains(Direction.Down) => 
-          agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Down)))
-        case KeyCode.Left.delegate if directions.contains(Direction.Left) => 
-          agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Left)))
-        case KeyCode.Right.delegate if directions.contains(Direction.Right) => 
-          agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Right)))
-        case KeyCode.Space.delegate =>
-          agent.makeMove(PartyMove(state.currentPlayer.id, DiceRoll, Option.empty[Direction]))
-        case _ => // Ignore other keys
+  def apply(event: KeyEvent, state: PartyState, agent: PlayingAgent): Unit =
+    if state.currentPlayer.id == agent.id then
+      val directions = state.possibleDirections.getOrElse(Set.empty[Direction])
+        state.phase match
+          case PartyPhase.PlayerMoving =>
+            event.getCode match
+              case KeyCode.Up.delegate if directions.contains(Direction.Up) => 
+                agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Up)))
+              case KeyCode.Down.delegate if directions.contains(Direction.Down) => 
+                agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Down)))
+              case KeyCode.Left.delegate if directions.contains(Direction.Left) => 
+                agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Left)))
+              case KeyCode.Right.delegate if directions.contains(Direction.Right) => 
+                agent.makeMove(PartyMove(state.currentPlayer.id, Movement, Some(Direction.Right)))
+              case _ =>
+          case PartyPhase.StartingRoll | PartyPhase.DiceRoll =>
+            event.getCode match
+              case KeyCode.Space.delegate =>
+                agent.makeMove(PartyMove(state.currentPlayer.id, DiceRoll, Option.empty[Direction]))
+              case _ =>
+          case _ =>
