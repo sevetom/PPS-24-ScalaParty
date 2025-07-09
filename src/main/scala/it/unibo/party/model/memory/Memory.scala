3bd0f78ffafe43dfb79a202458b98aa578c1a9e8
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-07-09 15:55:56 +0200

    feat: enhance Memory trait with layout definition and isEmpty logic

--- src/main/scala/it/unibo/party/model/memory/Memory.scala
+++ src/main/scala/it/unibo/party/model/memory/Memory.scala
@@ -3,11 +3,13 @@ package it.unibo.party.model.memory
 import it.unibo.party.geometry.Point2D
 
 trait Memory:
-  def isEmpty: Boolean = true
+  def layout: Map[Figure, (Point2D[Int], Point2D[Int])]
+
+  def isEmpty: Boolean = layout.isEmpty
 
 object Memory:
   def apply(data: Map[Figure, (Point2D[Int], Point2D[Int])]): Memory = MemoryImpl(data)
 
   def empty: Memory = Memory(Map.empty)
 
-  private case class MemoryImpl(map: Map[Figure, (Point2D[Int], Point2D[Int])]) extends Memory
+  private case class MemoryImpl(layout: Map[Figure, (Point2D[Int], Point2D[Int])]) extends Memory
--- src/test/scala/it/unibo/party/model/memory/MemoryTest.scala
+++ src/test/scala/it/unibo/party/model/memory/MemoryTest.scala
@@ -1,5 +1,6 @@
 package it.unibo.party.model.memory
 
+import it.unibo.party.geometry.Point2D
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 
@@ -7,3 +8,9 @@ class MemoryTest extends AnyFlatSpec:
   "The Memory" should "be empty when created" in:
     val memory = Memory(Map.empty)
     memory.isEmpty shouldBe true
+
+  it should "allow adding figures" in:
+    val point1 = Point2D(1, 2)
+    val point2 = Point2D(3, 4)
+    val memory = Memory(Map(Figure.Circle -> (point1, point2)))
+    memory.isEmpty shouldBe false
