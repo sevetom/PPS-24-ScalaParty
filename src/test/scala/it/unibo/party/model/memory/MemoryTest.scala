f013afe3a200e9b3de193bea173cac78ac6dc11e
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-07-09 15:57:50 +0200

    refactor: simplify MemoryTest with reusable variables for clarity

--- src/test/scala/it/unibo/party/model/memory/MemoryTest.scala
+++ src/test/scala/it/unibo/party/model/memory/MemoryTest.scala
@@ -5,12 +5,14 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 
 class MemoryTest extends AnyFlatSpec:
+  val emptyMemory: Memory = Memory(Map.empty)
+  val genericFigure: Figure = Figure.Circle
+  val genericCouplePoints: (Point2D[Int], Point2D[Int]) = (Point2D(0, 0), Point2D(1, 1))
+
   "The Memory" should "be empty when created" in:
-    val memory = Memory(Map.empty)
+    val memory = emptyMemory
     memory.isEmpty shouldBe true
 
   it should "allow adding figures" in:
-    val point1 = Point2D(1, 2)
-    val point2 = Point2D(3, 4)
-    val memory = Memory(Map(Figure.Circle -> (point1, point2)))
+    val memory = Memory(Map(genericFigure -> genericCouplePoints))
     memory.isEmpty shouldBe false
