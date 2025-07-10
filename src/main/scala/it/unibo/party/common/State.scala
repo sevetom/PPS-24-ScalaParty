309341f50549b94f70cdb942085ceae18a6aaa76
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-07-10 11:54:51 +0200

    feat: refactor MemoryController to manage game state and selections more effectively

--- src/main/scala/it/unibo/party/common/State.scala
+++ src/main/scala/it/unibo/party/common/State.scala
@@ -2,6 +2,7 @@ package it.unibo.party.common
 
 import it.unibo.party.geometry.{Direction, Point2D}
 import it.unibo.party.model.items.Collectable
+import it.unibo.party.model.memory.{Memory, MemoryBox}
 import it.unibo.party.model.partyGame.PartyGame
 import it.unibo.party.model.player.Pocket
 
@@ -14,7 +15,13 @@ trait MinigameState extends State:
   def winner: Option[Player]
 
 case class PuzzleState(phase: MinigamePhase, winner: Option[Player]) extends MinigameState
-case class MemoryState(phase: MinigamePhase, winner: Option[Player]) extends MinigameState
+case class MemoryState(
+                        game: Memory,
+                        phase: MinigamePhase,
+                        winner: Option[Player],
+                        firstSelection: Option[Point2D[Int]],
+                        mismatchedPair: Option[(Point2D[Int], Point2D[Int])] = None
+                      ) extends MinigameState
 case class MazeState(phase: MinigamePhase, winner: Option[Player]) extends MinigameState
 
 case class PartyState(
--- src/main/scala/it/unibo/party/controller/memory/MemoryController.scala
+++ src/main/scala/it/unibo/party/controller/memory/MemoryController.scala
@@ -9,54 +9,46 @@ import it.unibo.party.geometry.Point2D
 private val winTime = 30000L // 30 seconds
 
 object MemoryController:
-  def apply(game: Memory): MiniController = MemoryControllerImpl(game, 0L, winTime, MemoryState(MinigamePhase.Playing, None), 0)
+  def apply(game: Memory): MiniController = MemoryControllerImpl(MemoryState(game, MinigamePhase.Playing, None, None), 0L, winTime)
 
-  private case class MemoryControllerImpl(game: Memory,
+  private case class MemoryControllerImpl(state: MemoryState,
                                           startTime: Long,
-                                          winTime: Long,
-                                          state: MemoryState,
-                                          selected: Int) extends TimedMiniController:
+                                          winTime: Long) extends TimedMiniController:
 
     override def start(): MiniController =
-      val generatedMemory: Memory = game.generate
+      val generatedMemory: Memory = state.game.generate
       copy(
-        game = generatedMemory,
-        startTime = System.currentTimeMillis(),
-        state = MemoryState(MinigamePhase.Playing, None),
-        selected = 0
+        state = MemoryState(generatedMemory, MinigamePhase.Playing, None, None),
+        startTime = System.currentTimeMillis()
       )
 
     override def handleMove(move: Moves.Move): MiniController =
-      var firstSelection = Point2D[Int](0, 0)
       move match
         case MemoryMove.FlipCard(pos) =>
-          selected match
-            case 0 =>
-              firstSelection = pos
-              copy(
-                selected = 1
-              )
-            case 1 =>
-              val (isMatch, updatedGame) = game.check(MemoryBox(firstSelection), MemoryBox(pos))
+          val boxIsAlreadyShown = state.game.layout.values
+            .flatMap(p => List(p._1, p._2))
+            .find(_.pos == pos)
+            .exists(_.isShow)
+
+          if boxIsAlreadyShown || state.firstSelection.contains(pos) then
+            throw new IllegalArgumentException("This box is already shown or selected.")
+
+          state.firstSelection match
+            case None =>
+              val newState = state.copy(firstSelection = Some(pos), mismatchedPair = None)
+              copy(state = newState)
+
+            case Some(firstPos) =>
+              val coupleToCheck = (MemoryBox(firstPos), MemoryBox(pos))
+              val (isMatch, updatedGame) = state.game.check(coupleToCheck)
+
               if isMatch then
-                if updatedGame.isOver && System.currentTimeMillis() - startTime < winTime then
-                  copy(
-                    game = updatedGame,
-                    state = MemoryState(MinigamePhase.GameOver, Some(Player(0))),
-                    selected = 0
-                  )
-                else
-                  copy(
-                    game = updatedGame,
-                    state = MemoryState(MinigamePhase.Playing, None),
-                    selected = 0
-                  )
+                val winner = if updatedGame.isOver then Some(Player(0)) else None
+                val phase = if updatedGame.isOver then MinigamePhase.GameOver else MinigamePhase.Playing
+                val newState = state.copy(game = updatedGame, phase = phase, winner = winner, firstSelection = None)
+                copy(state = newState)
               else
-                copy(
-                  game = updatedGame,
-                  selected = 0
-                )
-            case _ =>
-              throw new IllegalStateException("Invalid selection state")
+                val newState = state.copy(firstSelection = None, mismatchedPair = Some((firstPos, pos)))
+                copy(state = newState)
 
 
