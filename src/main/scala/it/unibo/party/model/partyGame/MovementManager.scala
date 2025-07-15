3a85262d210668a563a68e9d7b88361610e4c612
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-07-15 11:47:50 +0200

    refactor: add Scaladoc comments to Dice, MovementManager, MovementResult, and PartyGame for better documentation

--- src/main/scala/it/unibo/party/model/partyGame/MovementManager.scala
+++ src/main/scala/it/unibo/party/model/partyGame/MovementManager.scala
@@ -6,18 +6,20 @@ import scala.util.Try
 
 given MovementManager:AnyRef with
   extension (pg: PartyGame)
+    /**
+     * Moves a pawn to a specified position on the board.
+     * @param pawnId The ID of the pawn to move.
+     * @param to The target position on the board.
+     * @return A MovementResult indicating the outcome of the move.
+     */
     def movePawn(pawnId: Int, to: BoardPosition): MovementResult =
-      Try {
+      Try:
         val oldPawn = pg.board.pawns(pawnId)
         val oldPocket = oldPawn.pocket.getAll
-  
         val newBoard = pg.board.movePawn(pawnId, to)
         val newPawn = newBoard.pawns(pawnId)
         val collected = newPawn.pocket.getAll.diff(oldPocket)
-  
-        // val availableDirs = newBoard.availableDirections(to)
-  
         val updatedGame = PartyGame(newBoard)(using pg.dice)
-        MovementResult.Moved(updatedGame/*, if availableDirs == Set.empty then None else Some(availableDirs)*/)
-      }.getOrElse(MovementResult.InvalidMove)
+        MovementResult.Moved(updatedGame)
+      .getOrElse(MovementResult.InvalidMove)
 
